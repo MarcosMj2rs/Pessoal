ac482d4cebdacc783a5a9997666551b87eebc07a
Marcos Jesus <marcos.mj2rs@gmail.com>
2026-04-30 03:43:26 -0300

    Configurado Docker / Kubernetes

--- BankMore/src/Services/Transferencia/Transferencia.API/Program.cs
+++ BankMore/src/Services/Transferencia/Transferencia.API/Program.cs
@@ -71,15 +71,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
         c.RoutePrefix = string.Empty;
     });
-}
+//}
 
 app.UseHttpsRedirection();
 
